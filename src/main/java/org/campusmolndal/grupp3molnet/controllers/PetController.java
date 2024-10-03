836758d2347436efd11d40d09614e9f89ce2b541
PontusIK <pontus.kavrestad@gmail.com>
2024-10-03 14:01:08 +0200

    tomma Crud metoder

--- src/main/java/org/campusmolndal/grupp3molnet/controllers/PetController.java
+++ src/main/java/org/campusmolndal/grupp3molnet/controllers/PetController.java
@@ -2,11 +2,49 @@ package org.campusmolndal.grupp3molnet.controllers;
 
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import org.campusmolndal.grupp3molnet.models.Pets;
+import org.campusmolndal.grupp3molnet.services.PetService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/pets")
 @RequiredArgsConstructor
 public class PetController {
+    private final PetService petService;
+
+    @PostMapping("")
+    public ResponseEntity<Pets> addPet(@RequestBody Pets pet) {
+        
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pets> getPetById(@PathVariable Long id) {
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Pets>> getAllPets() {
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Pets> updatePet(@PathVariable Long id, @RequestBody Pets pet) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePet(@PathVariable Long id) {
+        
+    }
 }
