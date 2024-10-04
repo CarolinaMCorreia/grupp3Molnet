14fe44cba71d54cb523afbc1c354bd9b6ebe403c
PontusIK <pontus.kavrestad@gmail.com>
2024-10-04 14:42:59 +0200

    user argument för att kontrollera behörighet för metoderna

--- src/main/java/org/campusmolndal/grupp3molnet/controllers/PetController.java
+++ src/main/java/org/campusmolndal/grupp3molnet/controllers/PetController.java
@@ -4,10 +4,12 @@ package org.campusmolndal.grupp3molnet.controllers;
 import lombok.RequiredArgsConstructor;
 
 import org.campusmolndal.grupp3molnet.models.Pet;
+import org.campusmolndal.grupp3molnet.models.Users;
 import org.campusmolndal.grupp3molnet.services.PetService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -23,8 +25,8 @@ public class PetController {
     private final PetService petService;
 
     @PostMapping("")
-    public ResponseEntity<Pet> addPet(@RequestBody Pet pet) {
-        return ResponseEntity.ok(petService.addPet(pet));
+    public ResponseEntity<Pet> addPet(@AuthenticationPrincipal Users user, @RequestBody Pet pet) {
+        return ResponseEntity.ok(petService.addPet(user, pet));
     }
 
     @GetMapping("/{id}")
@@ -38,13 +40,13 @@ public class PetController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Pet> updatePet(@PathVariable Long id, @RequestBody Pet pet) {
-        return ResponseEntity.ok(petService.updatePet(id, pet));
+    public ResponseEntity<Pet> updatePet(@AuthenticationPrincipal Users user, @PathVariable Long id, @RequestBody Pet pet) {
+        return ResponseEntity.ok(petService.updatePet(user, id, pet));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePet(@PathVariable Long id) {
-        petService.deletePet(id);
+    public ResponseEntity<Void> deletePet(@AuthenticationPrincipal Users user, @PathVariable Long id) {
+        petService.deletePet(user, id);
         return ResponseEntity.noContent().build();
     }
 }
