a7c99698103081fc30d3feeb039fdcb1e58cf73b
PontusIK <pontus.kavrestad@gmail.com>
2024-10-04 19:26:15 +0200

    find metoder + petrepository

--- src/main/java/org/campusmolndal/grupp3molnet/services/PetService.java
+++ src/main/java/org/campusmolndal/grupp3molnet/services/PetService.java
@@ -2,10 +2,14 @@ package org.campusmolndal.grupp3molnet.services;
 
 import org.campusmolndal.grupp3molnet.models.Pet;
 import org.campusmolndal.grupp3molnet.models.Users;
+import org.campusmolndal.grupp3molnet.repositories.PetRepository;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class PetService {
+    PetRepository petRepository;
 
     public Pet addPet(Users user, Pet pet) {
         // TODO Auto-generated method stub
@@ -13,13 +17,14 @@ public class PetService {
     }
 
     public Pet findPetById(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findPetById'");
+        return petRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("No pet found"));
     }
 
     public Iterable<Pet> findAllPets() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAllPets'");
+        Iterable<Pet> list = petRepository.findAll();
+        if (!list.iterator().hasNext()) throw new ResourceNotFoundException("No pets found");
+        return list;
     }
 
     public Pet updatePet(Users user, Long id, Pet pet) {
