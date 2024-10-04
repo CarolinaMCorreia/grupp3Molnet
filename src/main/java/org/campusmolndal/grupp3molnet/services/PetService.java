0d74adfcf1187e5b283fb4d049277ecbc58f2636
PontusIK <pontus.kavrestad@gmail.com>
2024-10-04 14:44:55 +0200

    tomma metoder

--- src/main/java/org/campusmolndal/grupp3molnet/services/PetService.java
+++ src/main/java/org/campusmolndal/grupp3molnet/services/PetService.java
@@ -1,7 +1,34 @@
 package org.campusmolndal.grupp3molnet.services;
 
+import org.campusmolndal.grupp3molnet.models.Pet;
+import org.campusmolndal.grupp3molnet.models.Users;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PetService {
+
+    public Pet addPet(Users user, Pet pet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addPet'");
+    }
+
+    public Pet findPetById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findPetById'");
+    }
+
+    public Iterable<Pet> findAllPets() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllPets'");
+    }
+
+    public Pet updatePet(Users user, Long id, Pet pet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updatePet'");
+    }
+
+    public void deletePet(Users user, Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletePet'");
+    }
 }
