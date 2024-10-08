55198ab4cbb39e5a9b6e591abfb0536806aeafff
Joakim Bagge <93joabag@gafe.molndal.se>
2024-10-08 14:58:04 +0200

    Ta bort attribut admin och password

--- src/main/java/org/campusmolndal/grupp3molnet/dtos/UserDto.java
+++ src/main/java/org/campusmolndal/grupp3molnet/dtos/UserDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.campusmolndal.grupp3molnet.models.Users;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @NoArgsConstructor
@@ -19,16 +20,8 @@ public class UserDto {
     @Schema(description = "The username", example = "johnDoe")
     private String username;
 
-    @Schema(description = "The password", example = "safestPassword123!")
-    private String password;
-
-    @Schema(description = "Indicates if the user has admin privileges", example = "false")
-    private boolean admin;
-
     public UserDto(Users user) {
         this.id = user.getUserId();
         this.username = user.getUsername();
-        this.password = user.getPassword();
-        this.admin = user.isAdmin();
     }
 }
