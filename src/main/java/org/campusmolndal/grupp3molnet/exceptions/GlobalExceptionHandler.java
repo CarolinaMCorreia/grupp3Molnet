fa6ce6c9e1d673663ac9e9878b957f0de96496e7
Joakim Bagge <93joabag@gafe.molndal.se>
2024-10-08 15:03:30 +0200

    Lägga till GlobalExceptionHandler

--- src/main/java/org/campusmolndal/grupp3molnet/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/campusmolndal/grupp3molnet/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package org.campusmolndal.grupp3molnet.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred.");
+    }
 }
