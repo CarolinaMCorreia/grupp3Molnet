8ed05dddc99a4af20891dc2d6c6d98c9ae2413e5
PontusIK <pontus.kavrestad@gmail.com>
2024-10-09 14:24:52 +0200

    ändrade till rätt import

--- src/main/java/org/campusmolndal/grupp3molnet/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/campusmolndal/grupp3molnet/exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package org.campusmolndal.grupp3molnet.exceptions;
 
-import java.nio.file.AccessDeniedException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
