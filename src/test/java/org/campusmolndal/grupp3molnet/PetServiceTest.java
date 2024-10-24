c81f5e3fe3d12746378644a205a503bd1a98ba5d
PontusIK <01ponifv@gafe.molndal.se>
2024-10-24 11:21:11 +0200

    addPet Test

--- src/test/java/org/campusmolndal/grupp3molnet/PetServiceTest.java
+++ src/test/java/org/campusmolndal/grupp3molnet/PetServiceTest.java
@@ -1,12 +1,23 @@
 package org.campusmolndal.grupp3molnet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDate;
+
+import org.campusmolndal.grupp3molnet.dtos.PetDto;
+import org.campusmolndal.grupp3molnet.models.Pet;
 import org.campusmolndal.grupp3molnet.repositories.PetRepository;
 import org.campusmolndal.grupp3molnet.services.PetService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.campusmolndal.grupp3molnet.models.Users;
+import org.campusmolndal.grupp3molnet.models.Pet.Species;
 
-public class PetServiceTest {
+class PetServiceTest {
     PetRepository petRepository;
     PetService petService;
 
@@ -14,5 +25,35 @@ public class PetServiceTest {
     void setup() {
         petRepository = mock(PetRepository.class);
         petService = new PetService(petRepository);
+    }
+
+    @Test
+    void addPet() {
+        Users caller = new Users(
+            1L,
+            "username",
+            "password",
+            false,
+            null
+        );
+
+        Pet pet = new Pet(
+            1L,
+            Species.DOG,
+            "Schäfer",
+            "namn",
+            LocalDate.now(),
+            null
+        );
+        
+        when(petRepository.save(any(Pet.class))).thenAnswer(invocation -> {
+            return invocation.getArgument(0);
+        }
+        );
+
+        PetDto actualDto = petService.addPet(caller, pet);
+
+        assertTrue(actualDto.getName().contains(pet.getName()));
+        assertEquals(caller.getUserId(), actualDto.getUserId());
     }
 }
